4b53fec64c5a0e3438641fbee6022c60075cbdec
guozhicheng <2399214024@qq.com>
2022-02-21 14:05:55 +0800

    Update WebSecurityConfig.java

--- eureka-security-server/src/main/java/com/gzc/cloud/config/WebSecurityConfig.java
+++ eureka-security-server/src/main/java/com/gzc/cloud/config/WebSecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 默认情况下添加 spring security 的应用的每个请求都需要添加 CSRF token 才能访问
+     * eureka 客户端注册并不会添加，所有需要配置 /eureka/** 路径不需要 CSRF token
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/eureka/**");

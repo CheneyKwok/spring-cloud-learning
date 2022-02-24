97bd2fc705736afe07bf24d17940074036491c95
guozhicheng <2399214024@qq.com>
2022-02-24 14:07:03 +0800

    Spring Cloud Admin

---
+++ admin-security-server/src/main/java/com/gzc/cloud/config/SecuritySecureConfig.java
@@ -0,0 +1,45 @@
+package com.gzc.cloud.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+        http.authorizeRequests()
+                 // 配置所有静态资源和登录页可以公开访问
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // 配置登录和登出页面
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler)
+                .and()
+                .logout().logoutUrl(adminContextPath + "/logout")
+                .and()
+                // 开启 http basic 支持，admin-client 注册时使用
+                .httpBasic()
+                .and()
+                // 开启基于 cookie 的 csrf 保护
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                // 忽略这些路径的 csrf 保护以便于 admin-client 注册
+                .ignoringAntMatchers(adminContextPath + "/instances", adminContextPath + "/actuator/**");
+    }
+}

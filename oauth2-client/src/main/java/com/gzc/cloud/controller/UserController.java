0576ef222074a020d7bbf8922ce6b1227d516a9f
guozhicheng <2399214024@qq.com>
2022-02-25 15:24:25 +0800

    oauth2 实现单点登录

--- oauth2-client/src/main/java/com/gzc/cloud/controller/UserController.java
+++ oauth2-client/src/main/java/com/gzc/cloud/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.gzc.cloud.controller;
 
-import cn.hutool.core.util.StrUtil;
-import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
+
 
 @RestController
 @RequestMapping("/user")
@@ -16,11 +14,6 @@ public class UserController {
 
     @GetMapping("/getCurrentUser")
     public Object getCurrentUser(Authentication authentication, HttpServletRequest request) {
-        String authorization = request.getHeader("Authorization");
-        String token = StrUtil.subAfter(authorization, "bearer", false);
-        return Jwts.parser()
-                .setSigningKey("test_key".getBytes(StandardCharsets.UTF_8))
-                .parseClaimsJws(token)
-                .getBody();
+        return authentication.getPrincipal();
     }
 }
--- oauth2-jwt-server/src/main/java/com/gzc/cloud/config/AuthorizationServerConfig.java
+++ oauth2-jwt-server/src/main/java/com/gzc/cloud/config/AuthorizationServerConfig.java
@@ -10,11 +10,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -67,14 +69,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .secret(passwordEncoder.encode("admin123456"))
                 // 配置访问 token 有效期
                 .accessTokenValiditySeconds(3600)
-                // 配置刷新 token 的时间
+                // 配置刷新 token 有效期
                 .refreshTokenValiditySeconds(864000)
                 // 配置 redirect_uri 用于授权成功后跳转
-                .redirectUris("http://www.baidu.com")
+//                .redirectUris("http://www.baidu.com")
+                .redirectUris("http://localhost:16000/login")
                 // 配置申请的权限范围
                 .scopes("all")
+                .autoApprove(true)
                 // 配置 grant_type 表示授权类型
-                .authorizedGrantTypes("authorization_code", "password");
+                .authorizedGrantTypes("authorization_code", "password", "refresh_token");
 
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("isAuthenticated()"); // 获取密钥需要身份认证，使用单点登录必须配置
     }
 }
